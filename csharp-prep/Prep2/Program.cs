950c7f8ac9d9a3650bf416ab1985912b39082a4f
Kalob Rust <kalobgavinrust@byui.edu>
2025-09-20 16:20:14 -0600

    prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your Grade.");
+        string x = Console.ReadLine();
+        int number = int.Parse(x);
+        if (number >= 90)
+        {
+            Console.WriteLine("Your Grade is an, A");
+        }
+        else if (number <= 89 && number >= 80)
+        {
+            Console.WriteLine("Your Grade is a, B");
+        }
+        else if (number <= 79 && number >= 70)
+        {
+            Console.WriteLine("Your Grade is a, C");
+        }
+        else if (number <= 69 && number >= 60)
+        {
+            Console.WriteLine("Your Grade is a, D");
+        }
+        else 
+        {
+            Console.WriteLine("You Failed the Class, F");
+        }
     }
 }
