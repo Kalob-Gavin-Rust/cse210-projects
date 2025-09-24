4227bd11959fd00ed5365da9e9edc1b65202fb6e
Kalob Rust <kalobgavinrust@byui.edu>
2025-09-24 14:12:56 -0600

    prep4 complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,27 @@
 using System;
-
+using System.Globalization;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
-}
+        List<int> numbers = new List<int>();
+        string response;
+        int x;
+        do
+        {
+            Console.WriteLine("Enter Number: ");
+            response = Console.ReadLine();
+            x = int.Parse(response);
+            numbers.Add(x);
+        } while (x != 0);
+
+        Console.WriteLine(".............................");
+        Console.WriteLine($"The Sum is: {numbers.Sum()}");
+        Console.WriteLine($"The Average is:  {numbers.Average()}");
+        Console.WriteLine($"The Maxium is: {numbers.Max()}");
+    } 
+} // I used ChatGPT to help me learn that I need 'System.Linq'-
+// for the math portion to work.
