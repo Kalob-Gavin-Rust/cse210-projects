cae7751d0d4774e9b1c458c7294177e5e9bbc190
Kalob Rust <kalobgavinrust@byui.edu>
2025-09-24 13:45:22 -0600

    Add range 1-100

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,7 +6,7 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 11);
+        int number = randomGenerator.Next(1, 101);
         bool guess;
         string response;
         guess = false;
@@ -36,7 +36,3 @@ class Program
 
     }
 }
-
-
-
-
