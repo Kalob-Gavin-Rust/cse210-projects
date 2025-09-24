0a67329b5afc39b1cec22fd79569dbb1576cbf48
Kalob Rust <kalobgavinrust@byui.edu>
2025-09-24 13:41:19 -0600

    Completed Prep 3, Guessing Game, Loop Practice

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        bool guess;
+        string response;
+        guess = false;
+        Console.WriteLine("Welcome to the Guessing Game!");
+        Console.WriteLine("The Goal is to guess a number at random!");
+        do
+        {
+            Console.WriteLine("What is your Guess?");
+            response = Console.ReadLine();
+            int x = int.Parse(response);
+            if (x > number && x != number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (x < number && x != number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                guess = true;
+            }
+
+        } while (guess == false);
+
+        Console.WriteLine("You Guessed It");
+
     }
 }
+
+
+
+
