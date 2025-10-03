cecfe2a4446f5a56b4789f83229bfa575300fd9c
Kalob Rust <kalobgavinrust@byui.edu>
2025-10-02 23:29:05 -0600

    learning 2 done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Linq;
+using System.Numerics;
 class Program
 {
     static void Main(string[] args)
@@ -17,11 +18,11 @@ class Program
             x = int.Parse(response);
             numbers.Add(x);
         } while (x != 0);
-
         Console.WriteLine(".............................");
         Console.WriteLine($"The Sum is: {numbers.Sum()}");
         Console.WriteLine($"The Average is:  {numbers.Average()}");
         Console.WriteLine($"The Maxium is: {numbers.Max()}");
+
     } 
 } // I used ChatGPT to help me learn that I need 'System.Linq'-
 // for the math portion to work.
