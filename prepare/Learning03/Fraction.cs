b7c46544cdec8ec0a4cd6df7c2073fd09f617699
Kalob Rust <kalobgavinrust@byui.edu>
2025-10-17 11:01:45 -0600

    Fractions Done

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,59 @@
+using System.Diagnostics;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string fs = $"{_top}/{_bottom}";
+        return fs;
+    }
+
+    public double GetDecimalValue()
+    {
+        double dec = (double)_top / (double)_bottom;
+        return dec;
+    }
+
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5, 1);
+        Fraction f3 = new Fraction(3, 4);
+
+        f1.GetTop();
+        f1.GetBottom();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        f2.GetTop();
+        f2.GetBottom();
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        f3.GetTop();
+        f3.GetBottom();
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+    
+        
     }
 }
