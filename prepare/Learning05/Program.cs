51a5d44b483f56454f885a2e1edde48afe6d3bd7
Kalob Rust <kalobgavinrust@byui.edu>
2025-11-13 18:23:59 -0700

    Shapes Complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Yellow",4);
+        shapes.Add(s1);
+        Rectangle s2 = new Rectangle(2,4,"Red");
+        shapes.Add(s2);
+        Circle s3 = new Circle(3,"Blue");
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
